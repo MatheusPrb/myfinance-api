85a2599e659d6ca011a2ad92c51712157a51d5fb
MatheusPrb <matheusparreirasb@gmail.com>
2026-04-01 00:26:06 -0300

    Update API route for user registration by removing the 'api' prefix for cleaner endpoint structure.

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\V1\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/api/v1/register', [AuthenticationController::class, 'register']);
+Route::post('/v1/register', [AuthenticationController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
